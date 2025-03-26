b3a007775c3e1eb31b4bdb808615415bdca9a87b
hoffe <djkagemand1@gmail.com>
2025-03-26 21:16:52 +0100

    test123

--- app/src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children, className }) => {
-  return (
-    <div className={`bg-white shadow-lg rounded-md p-5 ${className}`}>
-      {children}
-    </div>
-  );
-};
